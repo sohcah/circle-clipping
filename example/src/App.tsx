a69c1ec4a1d782d6cf7e01740fc690ba9892e0c1
Sam Hindess <mail@sohcah.dev>
2023-07-15 23:38:27 +0100

    Fix Polygon, Merging Issues, Performance Optimisations

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,16 +1,33 @@
 import './App.css'
-import MapGL, {Layer, Source} from 'react-map-gl/maplibre';
+import MapGL, {Layer, Source} from 'react-map-gl';
 import {
-    circle,
     destination,
-    Feature, point,
-    bearing, lineString, distance, polygon,
-} from '@turf/turf';
+    Feature,
+    bearing,
+    // distance,
+    polygon, area,
+} from "@turf/turf";
 import seedrandom from 'seed-random';
 import KDBush from "kdbush";
 import * as geokdbush from "geokdbush-tk";
+import CheapRuler from "cheap-ruler";
 
-const pointsPerCircle = 32;
+const rulers = new Map<number, CheapRuler>();
+
+function getRuler(lat: number): CheapRuler {
+    const key = Math.floor(lat * 10);
+    let ruler = rulers.get(key);
+    if (!ruler) {
+        ruler = new CheapRuler(lat, "kilometers");
+        rulers.set(key, ruler);
+    }
+    return ruler;
+}
+
+function distance([lon1, lat1]: [number, number], [lon2, lat2]: [number, number]): number {
+    return getRuler(lat1).distance([lon1, lat1], [lon2, lat2]);
+    // return haversine(toRadians(lat1), toRadians(lon1), toRadians(lat2), toRadians(lon2), earthRadius);
+}
 
 const data: [number, number, number][] = [
     [-122.444768, 37.782551, 0.03],
@@ -20,12 +37,15 @@ const data: [number, number, number][] = [
 ];
 
 const r = seedrandom("hi");
-//
-//
-for (let i = 0; i < 3000; i++) {
+
+// 9720: kinda broken
+// 9750: broken
+// for (let i = 0; i < 5000; i++) {
+//     data.push([data[0][0] + r() * 0.05, data[0][1] + r() * 0.05, r() * 0.1]);
+// }
+for (let i = 0; i < 10000; i++) {
     data.push([data[0][0] + r() * 0.05, data[0][1] + r() * 0.05, 0.03]);
 }
-console.log(r());
 
 interface IntersectionResult {
     points: [number, number][];
@@ -105,6 +125,7 @@ function haversine(lat1: number, lon1: number, lat2: number, lon2: number, radiu
     return radius * c;
 }
 
+
 function generateFlattened(circles: CircleArray[]): Feature[] {
     const features: Feature[] = [];
 
@@ -131,7 +152,7 @@ function generateFlattened(circles: CircleArray[]): Feature[] {
         for (const potentialIntersection of potentialIntersections) {
             const otherCircle = circles[potentialIntersection];
 
-            if (circle[2] + otherCircle[2] > distance(circle, otherCircle)) {
+            if (circle[2] + otherCircle[2] > distance([circle[0], circle[1]], [otherCircle[0], otherCircle[1]])) {
                 if (circleGroups[potentialIntersection] === undefined) {
                     circleGroups[potentialIntersection] = i;
                 } else {
@@ -214,7 +235,7 @@ function generateFlattened(circles: CircleArray[]): Feature[] {
             if (startAngle > endAngle) {
                 endAngle += 2 * Math.PI;
             }
-            const noPoints = Math.floor(Math.max(10, Math.abs(endAngle - startAngle) * 10));
+            const noPoints = Math.floor(Math.max(5, Math.abs(endAngle - startAngle) * 10));
             const deltaAngle = (endAngle - startAngle) / noPoints;
 
             const points: [number, number][] = [];
@@ -245,13 +266,6 @@ function generateFlattened(circles: CircleArray[]): Feature[] {
         }
     }
 
-    // for (const segment of arcSegments) {
-    //     if(!segment) continue;
-    //     features.push(lineString(segment, {
-    //         color: `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, "0")}`,
-    //     }));
-    // }
-
 
     const filteredSegmentsByGroup = arcSegments.filter(s => s !== null)
         .reduce((acc, segment) => {
@@ -269,16 +283,16 @@ function generateFlattened(circles: CircleArray[]): Feature[] {
     for (const group in filteredSegmentsByGroup) {
         const segments = filteredSegmentsByGroup[group];
         const groupPolygon: [number, number][][] = [];
-        let polygon: [number, number][] = [];
+        let currentPolygon: [number, number][] = [];
 
         let i = 0;
         while (segments.length) {
-            if (i++ > 1000) {
+            if (i++ > 1_000_000) {
                 throw new Error("Infinite loop");
             }
 
-            if (polygon.length === 0) {
-                polygon.push(...segments[0]);
+            if (currentPolygon.length === 0) {
+                currentPolygon.push(...segments[0]);
                 segments.splice(0, 1);
                 continue;
             }
@@ -288,92 +302,73 @@ function generateFlattened(circles: CircleArray[]): Feature[] {
                 const start = segment[0];
                 const end = segment[segment.length - 1];
 
-                const startDistance = distance(polygon[polygon.length - 1], start, {units: "kilometers"});
-                const endDistance = distance(polygon[0], end, {units: "kilometers"});
+                const startDistance = distance(currentPolygon[currentPolygon.length - 1], start, {units: "kilometers"});
+                const endDistance = distance(currentPolygon[0], end, {units: "kilometers"});
 
-                if (startDistance < 0.0001) {
-                    polygon.push(...segment);
+                if (startDistance < 0.0005) {
+                    currentPolygon.push(...segment);
                     found = true;
                     segments.splice(segments.indexOf(segment), 1);
                     break;
-                } else if (endDistance < 0.0001) {
-                    polygon.unshift(...segment);
+                } else if (endDistance < 0.0005) {
+                    currentPolygon.unshift(...segment);
                     found = true;
                     segments.splice(segments.indexOf(segment), 1);
                     break;
                 }
             }
-            if (!found) {
-                groupPolygon.push([...polygon, polygon[0]]);
-                polygon = [];
+            if (currentPolygon.length > 2 && distance(currentPolygon[0], currentPolygon[currentPolygon.length - 1], {units: "kilometers"}) < 0.0005) {
+                groupPolygon.push([...currentPolygon, currentPolygon[0]]);
+                currentPolygon = [];
+            } else if (!found) {
+                if (currentPolygon.length > 2 && distance(currentPolygon[0], currentPolygon[currentPolygon.length - 1], {units: "kilometers"}) < 0.05) {
+                    groupPolygon.push([...currentPolygon, currentPolygon[0]]);
+                    currentPolygon = [];
+                } else if (currentPolygon.length) {
+                    console.error("Could not find a segment to connect to", currentPolygon, segments, distance(currentPolygon[0], currentPolygon[currentPolygon.length - 1], {units: "kilometers"}));
+                    throw new Error("Could not find a segment to connect to");
+                }
             }
         }
 
-        if (polygon.length) {
-            groupPolygon.push([...polygon, polygon[0]]);
+        if (currentPolygon.length > 2 && distance(currentPolygon[0], currentPolygon[currentPolygon.length - 1], {units: "kilometers"}) < 0.05) {
+            groupPolygon.push([...currentPolygon, currentPolygon[0]]);
+            currentPolygon = [];
+        } else if (currentPolygon.length) {
+            console.error("Could not find a segment to connect to", currentPolygon, segments, distance(currentPolygon[0], currentPolygon[currentPolygon.length - 1], {units: "kilometers"}));
+            throw new Error("Could not find a segment to connect to");
         }
+
+        // Sort so that the largest polygon is first
+        const areasCache = new Map<unknown, number>();
+        const getArea = (poly: [number, number][]) => {
+            if (areasCache.has(poly)) {
+                return areasCache.get(poly)!;
+            }
+            const ar = area(polygon([poly]));
+            areasCache.set(poly, ar);
+            return ar;
+        }
+        groupPolygon.sort((a, b) => {
+            const areaA = getArea(a);
+            const areaB = getArea(b);
+            return areaB - areaA;
+        });
 
         polygons.push(groupPolygon);
     }
 
-    let changed = false;
-    while (changed) {
-        changed = false;
-
-        // Attempt to merge polygons
-        for (let i = 0; i < polygons.length; i++) {
-            const polygon1 = polygons[i];
-
-            for (let j = i + 1; j < polygons.length; j++) {
-                const polygon2 = polygons[j];
-
-                const startDistance = distance(polygon1[polygon1.length - 1], polygon2[0], {units: "kilometers"});
-                const endDistance = distance(polygon1[0], polygon2[polygon2.length - 1], {units: "kilometers"});
-
-                if (startDistance < 0.0001) {
-                    polygon1.push(...polygon2);
-                    polygons.splice(j, 1);
-                    changed = true;
-                    break;
-                } else if (endDistance < 0.0001) {
-                    polygon1.unshift(...polygon2);
-                    polygons.splice(j, 1);
-                    changed = true;
-                    break;
-                }
-            }
-            if (changed) break;
-        }
-    }
-
     // let i = 0;
     for (const poly of polygons) {
-        // const coords = [
-        //     ...poly,
-        //     poly[0],
-        // ];
-        // console.log(coords);
+        if (poly.length === 0) continue;
         features.push(polygon(poly, {
             color: `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, "0")}`,
         }));
-        // features.push(lineString(poly, {
-        //     color: `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, "0")}`,
-        //     label: `${i}`
-        // }));
-        // i++;
     }
-    // const params = features.map(i => [(i.geometry as any).coordinates]);
-    // const polys = xor(params[0], ...params.slice(1));
-    //
     const end = performance.now();
     console.log(`Time taken: ${end - start}ms`);
 
     return features;
-    //
-    //
-    // return polys.map((poly) => {
-    //     return polygon(poly);
-    // });
 }
 
 
@@ -385,48 +380,6 @@ function App() {
                 height: "100vh",
                 width: "100vw",
             }}>
-                {/*<div style={{flex: 1}}>*/}
-                {/*    <MapGL*/}
-                {/*        initialViewState={{*/}
-                {/*            longitude: -122.4,*/}
-                {/*            latitude: 37.8,*/}
-                {/*            zoom: 14*/}
-                {/*        }}*/}
-                {/*        mapStyle="https://api.maptiler.com/maps/streets/style.json?key=k02Kl04VqEmUthdcLti3"*/}
-                {/*    >*/}
-                {/*        <Source type="geojson" data={{*/}
-                {/*            type: "FeatureCollection",*/}
-                {/*            features: (() => {*/}
-                {/*                const circles = data.map((d) => circle([d[0], d[1]], d[2], {*/}
-                {/*                    steps: pointsPerCircle,*/}
-                {/*                }))*/}
-
-                {/*                return union(circles.map(i => i.geometry.coordinates as any)).map(i => ({*/}
-                {/*                    type: "Feature" as const,*/}
-                {/*                    geometry: {*/}
-                {/*                        type: "Polygon" as const,*/}
-                {/*                        properties: {},*/}
-                {/*                        coordinates: i,*/}
-                {/*                    },*/}
-                {/*                }));*/}
-                {/*            })()*/}
-                {/*        }}>*/}
-                {/*            <Layer type="fill" paint={{*/}
-                {/*                "fill-color": "#007cbf",*/}
-                {/*                "fill-opacity": 1*/}
-                {/*            }}/>*/}
-                {/*            <Layer type="line" paint={{*/}
-                {/*                "line-color": "#ad0000",*/}
-                {/*                "line-width": 2*/}
-                {/*            }}/>*/}
-
-                {/*            /!*<Layer type="circle" paint={{*!/*/}
-                {/*            /!*    "circle-radius": 4,*!/*/}
-                {/*            /!*    "circle-color": "#007cbf"*!/*/}
-                {/*            /!*}}/>*!/*/}
-                {/*        </Source>*/}
-                {/*    </MapGL>*/}
-                {/*</div>*/}
                 <div style={{flex: 1}}>
                     <MapGL
                         initialViewState={{
@@ -434,81 +387,14 @@ function App() {
                             latitude: 37.8,
                             zoom: 14
                         }}
-                        mapStyle="https://api.maptiler.com/maps/streets/style.json?key=k02Kl04VqEmUthdcLti3"
+                        mapboxAccessToken="pk.eyJ1Ijoic29oY2FoIiwiYSI6ImNqeWVqcm8wdTAxc2MzaXFpa282Yzd2aHEifQ.afYbt2sVMZ-kbwdx5_PekQ"
+                        mapStyle="mapbox://styles/mapbox/streets-v11"
+                        fog={{}}
+                        light={{}}
                     >
                         <Source type="geojson" data={{
                             type: "FeatureCollection",
-                            features: [
-                                [-122.39886460489092, 37.79642967019507, "97s", 1],
-                                [-122.39886460493965, 37.79642966916102, "97e", 2],
-                                [-122.39856795153895, 37.796908184045165, "53s", 3],
-                                [-122.39856795280795, 37.796908183694256, "53e", 4]
-                            ].map(d => point([d[0], d[1]], {
-                                label: d[2],
-                                offset: [0, d[3] * 2]
-                            }))
-                        }}>
-                            <Layer type="circle" paint={{
-                                "circle-radius": 10,
-                                "circle-color": "#007cbf"
-                            }}/>
-                            <Layer type="symbol"
-                                   layout={{
-                                       "text-field": ["get", "label"],
-                                       "text-size": 12,
-                                       "text-offset": ["get", "offset"]
-                                   }}
-                                   paint={{
-                                       "text-color": "#000000"
-                                   }}
-                            />
-                        </Source>
-                        {/*<Source type="geojson" data={{*/}
-                        {/*    type: "FeatureCollection",*/}
-                        {/*    features: data.map((d) => circle([d[0], d[1]], d[2], {*/}
-                        {/*        steps: pointsPerCircle,*/}
-                        {/*        units: "kilometers",*/}
-                        {/*    }))*/}
-                        {/*}}>*/}
-                        {/*    <Layer type="fill" paint={{*/}
-                        {/*        "fill-color": "#007cbf",*/}
-                        {/*        "fill-opacity": 0.1*/}
-                        {/*    }}/>*/}
-                        {/*</Source>*/}
-                        {/*<Source type="geojson" data={{*/}
-                        {/*    type: "FeatureCollection",*/}
-                        {/*    features: flatten(data)*/}
-                        {/*    // .map(d => d.length < 2 ? point(d[0]) : d.length < 4 ? lineString(d) : polygon(d))*/}
-                        {/*}}>*/}
-                        {/*    /!*<Layer type="line" paint={{*!/*/}
-                        {/*    /!*    "line-color": "#ad0000",*!/*/}
-                        {/*    /!*    "line-width": 2*!/*/}
-                        {/*    /!*}}/>*!/*/}
-
-                        {/*    /!*<Layer type="circle" paint={{*!/*/}
-                        {/*    /!*    "circle-radius": 4,*!/*/}
-                        {/*    /!*    "circle-color": "#007cbf"*!/*/}
-                        {/*    /!*}}/>*!/*/}
-                        {/*    <Layer type="circle" paint={{*/}
-                        {/*        "circle-radius": 3,*/}
-                        {/*        "circle-color": ["get", "color"],*/}
-                        {/*        "circle-opacity": ["get", "opacity"]*/}
-                        {/*    }}/>*/}
-                        {/*    <Layer type="line" paint={{*/}
-                        {/*        "line-color": ["get", "color"],*/}
-                        {/*        "line-width": 4,*/}
-                        {/*        "line-opacity": ["get", "opacity"]*/}
-                        {/*    }}/>*/}
-
-                        {/*    /!*<Layer type="fill" paint={{*!/*/}
-                        {/*    /!*    "fill-color": ["get", "color"],*!/*/}
-                        {/*    /!*    "fill-opacity": 0.5*!/*/}
-                        {/*    /!*}}/>*!/*/}
-                        {/*</Source>*/}
-                        <Source type="geojson" data={{
-                            type: "FeatureCollection",
                             features: generateFlattened(data)
-                            // .map(d => d.length < 2 ? point(d[0]) : d.length < 4 ? lineString(d) : polygon(d))
                         }}>
                             <Layer type="line"
                                    paint={{
@@ -519,21 +405,6 @@ function App() {
                                    paint={{
                                        "fill-color": ["get", "color"],
                                        "fill-opacity": 0.1
-                                   }}/>
-                            <Layer type="symbol"
-                                   layout={{
-                                       "text-field": ["get", "label"],
-                                       "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-                                       "text-offset": [0, 0.6],
-                                       "text-anchor": "top"
-                                   }}
-                                   paint={{
-                                       "text-color": "#000",
-                                   }}/>
-                            <Layer type="circle"
-                                   paint={{
-                                       "circle-color": ["get", "color"],
-                                       "circle-radius": ["get", "radius"],
                                    }}/>
                         </Source>
                     </MapGL>
